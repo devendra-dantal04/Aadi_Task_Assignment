9b9d9bffb401b82d08c1f7524400535c0ce3b611
Devendra Dantal <devendra.dantal04@gmail.com>
2024-07-09 16:53:24 +0530

    Facebook OAuth Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-
-import './App.css'
-import Login from './pages/Login'
+import { useEffect } from "react";
+import "./App.css";
+import Login from "./pages/Login";
 
 function App() {
+  useEffect(() => {
+  
+    window.fbAsyncInit = () => {
+      window.FB.init({
+          appId            : '395255233532264',
+          autoLogAppEvents : true,
+          xfbml            : true,
+          version          : 'v20.0'
+      });
+  };
+  (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { return; }
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  }, []);
 
   return (
     <>
       <Login />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,16 +2,28 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 
 function Login() {
+  const onLoginClick = () => {
+    window.FB.login(function (response) {
+      if (response.authResponse) {
+        console.log("Welcome!  Fetching your information.... ");
+        FB.api("/me", { fields: "name, email" }, function (response) {
+          document.getElementById("profile").innerHTML =
+            "Good to see you, " +
+            response.name +
+            ". i see your email address is " +
+            response.email;
+        });
+      } else {
+        console.log("User cancelled login or did not fully authorize.");
+      }
+    });
+  };
 
-    const onClick = () => {
-
-        checkLoginState();
-    }
-    return <div>
-        <Button onClick={onClick}>
-            Login with facebook
-        </Button>
-    </div>;
+  return (
+    <div>
+      <Button onClick={onLoginClick}>Login with facebook</Button>
+    </div>
+  );
 }
 
 export default Login;
