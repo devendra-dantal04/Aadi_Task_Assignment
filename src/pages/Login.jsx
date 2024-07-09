5b26ac182315842d8457f183c4276184b30f37ae
Devendra Dantal <devendra.dantal04@gmail.com>
2024-07-09 17:20:02 +0530

    Add Response

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,14 +5,17 @@ function Login() {
   const onLoginClick = () => {
     window.FB.login(
       function (response) {
+        console.log(response);
         if (response.authResponse) {
           console.log("Welcome!  Fetching your information.... ");
           FB.api("/me", { fields: "name, email" }, function (response) {
-            document.getElementById("profile").innerHTML =
-              "Good to see you, " +
-              response.name +
-              ". i see your email address is " +
-              response.email;
+            // document.getElementById("profile").innerHTML =
+            //   "Good to see you, " +
+            //   response.name +
+            //   ". i see your email address is " +
+            //   response.email;
+
+            console.log(response.name, response.email);
           });
         } else {
           console.log("User cancelled login or did not fully authorize.");
